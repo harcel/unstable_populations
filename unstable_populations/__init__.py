fb91b0e6cf1b9557207635b21329c342870d53a1
Haas <m.r.haas@lumc.nl>
2023-09-11 21:56:28 +0200

    clean_package

---
+++ unstable_populations/__init__.py
@@ -0,0 +1,7 @@
+from .unstable_populations import upi
+from .unstable_populations import psi
+from .unstable_populations import KL
+
+__version__ = "0.2.1"
+__author__ = "Marcel R. Haas, Joris Huese, Lisette Sibbald"
+__credits__ = "Data Science Center, University of Amsterdam"
